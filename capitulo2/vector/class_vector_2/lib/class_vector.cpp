c3fc2bf9be15d6a19360f7be7913fc1c2128f8da
Jesus Adrian <elbuho12@outlook.com>
2024-09-01 12:56:09 +0200

    class_vector_2

---
+++ capitulo2/vector/class_vector_2/lib/class_vector.cpp
@@ -0,0 +1,56 @@
+#include "class_vector.h"
+#include <initializer_list>
+#include <utility>
+Vector::Vector(std::initializer_list<double> lst)
+:elem{new double[lst.size()]}, sz{lst.size()}
+{
+  std::copy(lst.begin(),lst.end(),elem);
+}
+Vector::Vector()
+  :sz{0}
+{}
+Vector::~Vector()
+{
+  delete[] elem;
+}
+double& Vector::operator[](int i)
+{
+  return elem[i];
+}
+
+int Vector::size()
+{
+ return sz;
+}
+
+void Vector::push_back(double d)
+{
+  sz++;
+  double *arr=new double[sz];
+  if(sz>1)
+  {
+    for(int i = 0; i != sz; i++)
+    {
+      if (i==(sz-1))
+      {
+        arr[i]=d;
+      }else{
+        arr[i]=elem[i];
+      }
+    }
+    delete[] elem;
+  }else{
+    arr[0]=d;
+  }
+  elem=std::move(arr);
+}
+
+Vector read(std::istream &is)
+{
+ Vector v;
+ for (double d; is>>d;) 
+ {
+  v.push_back(d);
+ }
+ return v;
+}
